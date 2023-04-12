6811da095b142e5830621ada7dfe4d24b55bf394
Jhon98E <jhonenriquez1998@gmail.com>
2023-04-11 21:17:14 -0500

    Calculadora

---
+++ Unidad1/clase-4-Jhon-David/src/App.js
@@ -0,0 +1,99 @@
+import './App.scss';
+import Button from 'react-bootstrap/Button';
+import Form from 'react-bootstrap/Form';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Container } from 'react-bootstrap';
+import { useState } from 'react';
+
+
+function App() {
+
+  const [formulario, setformulario] = useState({
+    email: '',
+    password: ''
+  });
+
+  const handleChange = (event) => {
+    const temporal = {...formulario}
+    temporal[event.target.name] = event.target.value
+    setformulario(temporal)
+  }
+
+  return (
+    <div className="App">
+      <Container>
+        <Form>
+          <Form.Group>
+            <Form.Label>Email address</Form.Label>
+            <Form.Control onchange={handleChange} name="email" type="email" placeholder="Enter email" />
+            <Form.Text className="text-muted">
+              We'll never share your email with anyone else.
+            </Form.Text>
+          </Form.Group>
+
+          <Form.Group>
+            <Form.Label>Password</Form.Label>
+            <Form.Control onchange={handleChange} name="password" type="password" placeholder="Password" />
+          </Form.Group>
+          {formulario.email}
+          {formulario.password}
+          <Button>
+            Submit
+          </Button>
+
+          <Row>
+            
+            <Col>
+
+              <Form.Group>
+                <Form.Label>NUMERO 1</Form.Label>
+                <Form.Control placeholder="Ingrese un Numero"></Form.Control>
+              </Form.Group>
+
+              <Form.Group>
+                <Form.Label>NUMERO 2</Form.Label>
+                <Form.Control placeholder="Ingrese un Numero"></Form.Control>
+              </Form.Group>
+
+              <Form.Group>
+                <Form.Label>RESULTADO</Form.Label>
+                <Form.Control placeholder=""></Form.Control>
+              </Form.Group>
+
+            </Col>
+
+            <Col>
+
+              <Form.Group>
+                <Form.Label>SUMA</Form.Label>
+                <Form.Control placeholder="+"></Form.Control>
+              </Form.Group>
+
+              <Form.Group>
+                <Form.Label>RESTA</Form.Label>
+                <Form.Control placeholder="-"></Form.Control>
+              </Form.Group>
+
+              <Form.Group>
+                <Form.Label>MULTIPLICACION</Form.Label>
+                <Form.Control placeholder="*"></Form.Control>
+              </Form.Group>
+
+              <Form.Group>
+                <Form.Label>DIVISION</Form.Label>
+                <Form.Control placeholder="/"></Form.Control>
+              </Form.Group>
+
+            </Col>
+
+          </Row>
+
+        </Form>
+      </Container>
+    </div>
+  );
+}
+
+export default App;
