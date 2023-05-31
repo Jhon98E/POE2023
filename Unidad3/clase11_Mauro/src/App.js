5c078ac4cd631de8f35cb4cf29337933ecc05493
MauroSk8 <mauroprieto2001@gmail.com>
2023-05-31 00:11:23 -0500

    Cambios

---
+++ Unidad3/clase11_Mauro/src/App.js
@@ -0,0 +1,52 @@
+import './App.css';
+import Button from 'react-bootstrap/Button';
+import Form from 'react-bootstrap/Form';
+import { useState } from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css'
+import './servicios/clienteAxios'
+function App() {
+
+  const [formulario, setformulario] = useState({
+    titulo: '',
+    resumen: ''
+  });
+
+  const handleChange = (event) => {
+    const temporal = {...formulario} //Copia el objeto quitando la referencia
+    temporal[event.target.name] = event.target.value
+    setformulario(temporal)
+  }
+
+  return (
+    <div className="App">
+      <div class="container">
+        <form id="formulario">
+        <h3>Libreria</h3>
+          <div class="row">
+
+            <div class="col">
+
+              <Form.Group className="mb-3">
+                <Form.Text className="text-muted"># Ingrese los datos del libro</Form.Text>
+                <br></br>
+                <Form.Label>Titulo</Form.Label>
+                <Form.Control onChange={handleChange} name="titulo" placeholder="Ingrese titulo"></Form.Control>
+              </Form.Group>
+
+              <Form.Group className="mb-3" >
+                <Form.Label>Resumen</Form.Label>
+                <Form.Control onChange={handleChange} name="resumen" placeholder="Ingrese el resumen"></Form.Control>
+              </Form.Group>
+              <Button onClick={guardarLibro}>
+                Guargar Libro
+              </Button>
+
+            </div>
+          </div>
+        </form>
+      </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ Unidad3/clase11_Mauro/src/servicios/clienteAxios.js
@@ -0,0 +1,24 @@
+import axios from "axios";
+
+const baseurl = "http://localhost:8000/api/";
+
+export const crearConfiguracion = (metodo, url, informacion) => {
+    return{
+        method: metodo,
+        url: baseurl + url,
+        data: informacion
+    }
+}
+
+export const hacerPeticion = async (informacion) => {
+    try {
+        const configuracion = crearConfiguracion("post", "biblioteca/", informacion)
+        const respuesta = await axios(configuracion)
+        if (respuesta == 201) {
+            console.log(respuesta)
+        }
+        return respuesta.status;
+    } catch(error) {
+        console.log(error)
+    }
+}
